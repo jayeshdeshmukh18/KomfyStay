e04f2b97e1e0dc174533924d3fb0d09db664e084
jayeshdeshmukh18 <deshmukhjayesh507@gmail.com>
2023-09-02 18:00:06 +0530

    changes: readme file

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const moment = require('moment');
+const path = require('path');
+const app = express();
+const session = require('express-session');
+require('dotenv').config();
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+mongoose
+	.connect(process.env.DB_URI)
+	.then(() => {
+		console.log('db started');
+	})
+	.catch((error) => {
+		console.log(error);
+	});
+
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: true,
+		cookie: {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 2
+			// secure: true
+		}
+	})
+);
+
+const User = require('./models/user');
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.set('view engine', 'ejs');
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+	res.locals.moment = moment;
+	next();
+});
+
+const hotelRoutes = require('./routes/hotels');
+const reviewRoutes = require('./routes/reviews');
+const authRoutes = require('./routes/auth');
+app.use(hotelRoutes);
+app.use(reviewRoutes);
+app.use(authRoutes);
+
+const port = process.env.PORT;
+app.listen(port, () => {
+	console.log('server started');
+});
---
+++ routes/hotels.js
@@ -0,0 +1,141 @@
+const express = require('express');
+const router = express.Router();
+
+const Hotel = require('../models/hotel');
+
+// CLOUDINARY
+const multer = require('multer');
+const storage = require('../cloudinary/index');
+const upload = multer({ storage });
+// MAPBOX
+const geocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocodingClient = geocoding({ accessToken: process.env.MAPBOX_TOKEN });
+// CONTACT FORM
+const { sendEmail } = require('../middlewares/email');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+router.get('/', (req, res) => {
+	res.render('landing', { page: 'Home - StaySense' });
+});
+router.get('/contact', (req, res) => {
+	res.render('contact', { page: 'Contact' });
+});
+router.post('/contact', async (req, res) => {
+	try {
+		await sendEmail(req.body.contact);
+		res.redirect('/contact');
+	} catch (error) {
+		res.send(error);
+	}
+});
+router.get('/hotels', async (req, res) => {
+	try {
+		const hotels = await Hotel.find();
+		res.render('hotels/index', { hotels, page: 'All Hotels - StaySense' });
+	} catch (error) {
+		res.send(error);
+	}
+});
+router.get('/hotels/new', (req, res) => {
+	res.render('hotels/new', { page: 'New Hotel - StaySense' });
+});
+router.post('/hotels', upload.array('image'), async (req, res) => {
+	try {
+		const newHotel = new Hotel(req.body.hotel);
+		// req.files -> array -> index: path
+		// console.log(req.files);
+		for (let img of req.files) {
+			newHotel.image.push(img.path);
+		}
+		// const query = req.body.hotel.address;
+		// const result = await geocodingClient
+		// 	.forwardGeocode({
+		// 		query,
+		// 		limit: 1
+		// 	})
+		// 	.send();
+		// newHotel.location = result.body.features[0].geometry;
+		await newHotel.save();
+		res.redirect(`/hotels/${newHotel._id}`);
+		
+	} catch (error) {
+		res.send(error);
+	}
+});
+router.get('/hotels/:id', async (req, res) => {
+	try {
+		const hotel = await Hotel.findById(req.params.id).populate('reviews');
+		const reviews = hotel.reviews;
+		res.render('hotels/show', { reviews, hotel, page: 'Hotel Details - StaySense' });
+	} catch (error) {
+		res.send(error);
+	}
+});
+router.get('/hotels/:id/edit', async (req, res) => {
+	try {
+		const hotel = await Hotel.findById(req.params.id);
+		res.render('hotels/edit', { hotel, page: 'Edit Hotel - StaySense' });
+	} catch (error) {
+		res.send(error);
+	}
+});
+router.patch('/hotels/:id', async (req, res) => {
+	try {
+		const hotel = await Hotel.findByIdAndUpdate(req.params.id, req.body.hotel);
+		const query = req.body.hotel.address;
+		const result = await geocodingClient
+			.forwardGeocode({
+				query,
+				limit: 1
+			})
+			.send();
+		hotel.location = result.body.features[0].geometry;
+		await hotel.save();
+		res.redirect(`/hotels/${req.params.id}`);
+	} catch (error) {
+		res.send(error);
+	}
+});
+router.delete('/hotels/:id', async (req, res) => {
+	try {
+		await Hotel.findByIdAndRemove(req.params.id);
+		res.redirect('/hotels');
+	} catch (error) {
+		res.send(error);
+	}
+});
+router.get('/hotels/:id/checkout', async (req, res) => {
+	try {
+		const hotel = await Hotel.findById(req.params.id);
+		const session = await stripe.checkout.sessions.create({ 
+			payment_method_types: ["card"], 
+			line_items: [ 
+			  { 
+				price_data: { 
+					currency: "inr", 
+					product_data: { 
+						name: hotel.name, 
+						description: hotel.address,
+						images: [hotel.image[0]]
+					}, 
+					unit_amount: hotel.price * 100, 
+				}, 
+				quantity: 1, 
+			  }, 
+			], 
+			mode: "payment", 
+			success_url: "http://localhost:3000/success", 
+			cancel_url: "http://localhost:3000/cancel", 
+		}); 
+		res.redirect(session.url);
+	} catch (error) {
+		res.send(error);
+	}
+});
+router.get('/success', (req, res)=>{
+	res.send('payment successful');
+})
+router.get('/cancel', (req, res)=>{
+	res.send('payment cancelled');
+})
+module.exports = router;
